943192baf688dd1179f6b162af49b456b4d25073
Chris Lewis <clewis@palantir.com>
2019-12-20 23:53:15 -0800

    Add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { InputGroup, FormGroup, ControlGroup, Button, Intent, Classes } from "@blueprintjs/core";
+import { InputGroup, FormGroup, ControlGroup, Button, Intent, Classes, ButtonGroup } from "@blueprintjs/core";
 import "./App.scss";
 import { Pitch, IChord, harmonize } from "./util/harmonize";
 
+const PAGE_SIZE = 50;
+
 interface IAppState {
   melodyInput: string;
+  pageIndex: number;
   results: IChord[][] | undefined;
 }
 
 export class App extends React.PureComponent {
   public state: IAppState = {
-    melodyInput: "",
+    melodyInput: "E D C D E",
+    pageIndex: 0,
     results: undefined,
   };
 
@@ -21,6 +25,7 @@ export class App extends React.PureComponent {
         <FormGroup label="Enter a melody in C major:" helperText="Put a space between each pitch name. Accidentals like F# and Bb are supported.">
           <ControlGroup fill={true}>
             <InputGroup
+              large={true}
               onChange={this.handleMelodyInputChange}
               placeholder="Example: E D C D E E E D D D E C C"
               value={this.state.melodyInput}
@@ -29,6 +34,7 @@ export class App extends React.PureComponent {
               className={Classes.FIXED}
               intent={Intent.PRIMARY}
               icon="music"
+              large={true}
               onClick={this.handleButtonClick}
             >
               Harmonize!
@@ -58,19 +64,21 @@ export class App extends React.PureComponent {
   };
 
   private maybeRenderResults() {
-    const { results } = this.state;
+    const { pageIndex, results } = this.state;
     if (results === undefined) {
       return undefined;
     }
 
-    const firstFewResults = results.slice(0, 50);
+    const firstFewResults = results.slice(pageIndex * PAGE_SIZE, pageIndex * PAGE_SIZE + PAGE_SIZE);
     const resultItems = firstFewResults.map(this.renderResultItem);
 
-    const resultsUnit = resultItems.length === 1 ? "way" : "ways";
+    const isAre = resultItems.length === 1 ? "is" : "are";
+    const ways = resultItems.length === 1 ? "way" : "ways";
 
     return (
       <div>
-        <p><strong>Here are {resultItems.length} {resultsUnit} to harmonize this melody:</strong></p>
+        <p><strong>There {isAre} {results.length}+ {ways} to harmonize this melody.</strong></p>
+        {this.renderPaginationControls(results)}
         {resultItems}
       </div>
     );
@@ -90,4 +98,32 @@ export class App extends React.PureComponent {
 
     return <div className="hz-chord-result" key={resultStr}>{elements}</div>;
   }
+
+  private renderPaginationControls(allResults: IChord[][]) {
+    const { pageIndex } = this.state;
+    const numPages = Math.ceil(allResults.length / PAGE_SIZE);
+
+    const pageButtons: JSX.Element[] = [];
+    for (let i = 0; i < numPages; i++) {
+      pageButtons.push(<Button active={i === pageIndex} onClick={() => this.setState({ pageIndex: i })}>{i + 1}</Button>)
+    }
+
+    return (
+      <div className="hz-pagination-controls">
+        <ButtonGroup>
+          <Button disabled={pageIndex === 0} intent={Intent.PRIMARY} onClick={this.handlePrevButtonClick}>Prev</Button>
+          {pageButtons}
+          <Button disabled={pageIndex === numPages - 1} intent={Intent.PRIMARY} onClick={this.handleNextButtonClick}>Next</Button>
+        </ButtonGroup>
+      </div>
+    );
+  }
+
+  private handlePrevButtonClick = () => {
+    this.setState({ pageIndex: this.state.pageIndex - 1 });
+  };
+
+  private handleNextButtonClick = () => {
+    this.setState({ pageIndex: this.state.pageIndex + 1 });
+  };
 }
