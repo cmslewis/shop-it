8c9ea1eae13ab4af62be6987359149f242a8a81f
Chris Lewis <clewis@palantir.com>
2019-12-21 11:08:41 -0800

    Refactor: ResultsPane component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from "react";
-import { InputGroup, FormGroup, ControlGroup, Button, Intent, Classes, ButtonGroup, Card } from "@blueprintjs/core";
+import { InputGroup, FormGroup, ControlGroup, Button, Intent, Classes } from "@blueprintjs/core";
 import "./App.scss";
-import { PaginationControls } from "./components/PaginationControls";
+import { ResultsPane } from "./components/ResultsPane";
 import { Pitch, IChord, harmonize } from "./util/harmonize";
-import { pageGetResultStartIndex, pageGetResultEndIndex } from "./util/pagingUtils";
-import { formatInteger } from "./util/numberUtils";
 
-const DEFAULT_PAGE_SIZE = 50;
 const ENTER_KEY = 13;
 
 interface IAppState {
   melodyInput: string;
-  pageIndex: number;
-  pageSize: number;
   results: IChord[][] | undefined;
 }
 
 export class App extends React.PureComponent {
   public state: IAppState = {
     melodyInput: "E D C D E",
-    pageIndex: 0,
-    pageSize: DEFAULT_PAGE_SIZE,
     results: undefined,
   };
 
@@ -77,76 +70,7 @@ export class App extends React.PureComponent {
   };
 
   private maybeRenderResults() {
-    const { pageIndex, pageSize, results } = this.state;
-
-    if (results === undefined) {
-      return undefined;
-    }
-
-    const pageStart = pageGetResultStartIndex(pageIndex, pageSize);
-    const pageEndExclusive = pageGetResultEndIndex(pageIndex, pageSize, results.length);
-
-    const pageResultItems = results.slice(pageStart, pageEndExclusive).map(this.renderResultItem);
-
-    const isAre = pageResultItems.length === 1 ? "is" : "are";
-    const ways = pageResultItems.length === 1 ? "way" : "ways";
-
-    return (
-      <Card>
-        <p><strong>Here {isAre} {formatInteger(results.length)} {ways} to harmonize this melody.</strong></p>
-        {this.renderPaginationControls()}
-        <div className="hz-chord-results">
-          {pageResultItems}
-        </div>
-        {this.renderPaginationControls(false)}
-      </Card>
-    );
+    const { results } = this.state;
+    return results === undefined ? undefined : <ResultsPane results={results}></ResultsPane>;
   }
-
-  private renderResultItem = (result: IChord[], index: number) => {
-    const { pageIndex, pageSize } = this.state;
-
-    const resultAbsoluteIndex = pageGetResultStartIndex(pageIndex, pageSize) + index;
-
-    const resultStr = result.map(c => c.name).join(" ");
-    const chordNames = result.map((c, i) => <span key={i} className="hz-chord-name">{c.name}</span>);
-
-    const elements = [];
-    for (let i = 0; i < chordNames.length; i++) {
-      if (i > 0) {
-        elements.push(<span key={i + 0.5} className="hz-chord-arrow">→</span>)
-      }
-      elements.push(chordNames[i]);
-    }
-
-    return (
-      <div className="hz-chord-result" key={resultStr}>
-        {/* 1-indexed */}
-        <div className="hz-chord-result-id">#{resultAbsoluteIndex + 1}</div>
-        {elements}
-      </div>
-    );
-  }
-
-  private renderPaginationControls(withBottomMargin = true) {
-    const { pageIndex, pageSize, results } = this.state;
-
-    if (results === undefined) {
-      return undefined;
-    }
-
-    return (
-      <PaginationControls
-        numResults={results.length}
-        pageIndex={pageIndex}
-        pageSize={pageSize}
-        onPageChange={this.handlePageChange}
-        onPageSizeChange={this.handlePageSizeChange}
-        withBottomMargin={withBottomMargin}
-      />
-    );
-  }
-
-  private handlePageChange = (pageIndex: number) => this.setState({ pageIndex });
-  private handlePageSizeChange = (pageSize: number) => this.setState({ pageSize });
 }
---
+++ src/components/ResultsPane.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import { Card } from "@blueprintjs/core";
+import { IChord } from "../util/harmonize";
+import { formatInteger } from "../util/numberUtils";
+import { pageGetResultStartIndex, pageGetResultEndIndex } from "../util/pagingUtils";
+import { PaginationControls } from "./PaginationControls";
+
+const DEFAULT_PAGE_SIZE = 50;
+
+export interface IResultsPaneProps {
+  results: IChord[][];
+}
+
+export interface IResultsPaneState {
+  pageIndex: number;
+  pageSize: number;
+}
+
+export class ResultsPane extends React.PureComponent<IResultsPaneProps, IResultsPaneState> {
+  public state: IResultsPaneState = {
+    pageIndex: 0,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  public render() {
+    const { results } = this.props;
+    const { pageIndex, pageSize } = this.state;
+
+    const pageStart = pageGetResultStartIndex(pageIndex, pageSize);
+    const pageEndExclusive = pageGetResultEndIndex(pageIndex, pageSize, results.length);
+
+    const pageResultItems = results.slice(pageStart, pageEndExclusive).map(this.renderResultItem);
+
+    const isAre = pageResultItems.length === 1 ? "is" : "are";
+    const ways = pageResultItems.length === 1 ? "way" : "ways";
+
+    return (
+      <Card>
+        <p><strong>Here {isAre} {formatInteger(results.length)} {ways} to harmonize this melody.</strong></p>
+        {this.renderPaginationControls()}
+        <div className="hz-chord-results">
+          {pageResultItems}
+        </div>
+        {this.renderPaginationControls(false)}
+      </Card>
+    );
+  }
+
+  private renderResultItem = (result: IChord[], index: number) => {
+    const { pageIndex, pageSize } = this.state;
+
+    const resultAbsoluteIndex = pageGetResultStartIndex(pageIndex, pageSize) + index;
+
+    const resultStr = result.map(c => c.name).join(" ");
+    const chordNames = result.map((c, i) => <span key={i} className="hz-chord-name">{c.name}</span>);
+
+    const elements = [];
+    for (let i = 0; i < chordNames.length; i++) {
+      if (i > 0) {
+        elements.push(<span key={i + 0.5} className="hz-chord-arrow">→</span>)
+      }
+      elements.push(chordNames[i]);
+    }
+
+    return (
+      <div className="hz-chord-result" key={resultStr}>
+        {/* 1-indexed */}
+        <div className="hz-chord-result-id">#{resultAbsoluteIndex + 1}</div>
+        {elements}
+      </div>
+    );
+  }
+
+  private renderPaginationControls(withBottomMargin = true) {
+    const { results } = this.props;
+    const { pageIndex, pageSize } = this.state;
+
+    if (results === undefined) {
+      return undefined;
+    }
+
+    return (
+      <PaginationControls
+        numResults={results.length}
+        pageIndex={pageIndex}
+        pageSize={pageSize}
+        onPageChange={this.handlePageChange}
+        onPageSizeChange={this.handlePageSizeChange}
+        withBottomMargin={withBottomMargin}
+      />
+    );
+  }
+
+  private handlePageChange = (pageIndex: number) => this.setState({ pageIndex });
+  private handlePageSizeChange = (pageSize: number) => this.setState({ pageSize });
+}
