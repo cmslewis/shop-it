1d4c0302e1d8edb91f3d10eb3174ba81e25d1894
Chris Lewis <clewis@palantir.com>
2019-12-22 00:21:23 -0800

    Updates

---
+++ precache-manifest.466a49231b7d57c68953303690e03259.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca959378ebfe59c5bf9666613fb3caf",
+    "url": "/shop-it/index.html"
+  },
+  {
+    "revision": "8cf8bb29759b91573125",
+    "url": "/shop-it/static/css/2.ef050e13.chunk.css"
+  },
+  {
+    "revision": "1b4b9e785370eed76302",
+    "url": "/shop-it/static/css/main.c2d6ef94.chunk.css"
+  },
+  {
+    "revision": "8cf8bb29759b91573125",
+    "url": "/shop-it/static/js/2.591cc1d6.chunk.js"
+  },
+  {
+    "revision": "003123287f46f806c1333aee1d43abc6",
+    "url": "/shop-it/static/js/2.591cc1d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b4b9e785370eed76302",
+    "url": "/shop-it/static/js/main.d1978670.chunk.js"
+  },
+  {
+    "revision": "8aff55bd6bb755dfdc7d",
+    "url": "/shop-it/static/js/runtime-main.d599c8d0.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/shop-it/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/shop-it/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/shop-it/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/shop-it/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/shop-it/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/shop-it/static/media/icons-20.ca7934b3.eot"
+  }
+]);
